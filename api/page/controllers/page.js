e9d377f805ec87d269bd5ffd643a819b70ece45d
Petr Borak <petr.borak@moneta.cz>
2020-11-19 23:11:31 +0100

    [BORAKPE1] - initial commit

---
+++ api/page/controllers/page.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+  
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.page.create(data, { files });
+    } else {
+      entity = await strapi.services.page.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.page });
+  },
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'acec6e8e-58ec-4c97-9615-43032ef333d8'
+};
